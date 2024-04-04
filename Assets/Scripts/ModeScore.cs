3d1cd580f3c5a0082fd2a32d8c78bb401d6b6d24
Yaakulya Sabbani <134747002+yaakulya123@users.noreply.github.com>
2024-04-04 19:23:22 +0400

    Added files

---
+++ Assets/Scripts/ModeScore.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModeScore : MonoBehaviour {
+
+    public int ModeSelection;
+    public GameObject RaceUI;
+    public GameObject ScoreUI;
+    public GameObject AICar;
+    public static int CurrentScore;
+    public int InternalScore;
+    public GameObject ScoreValue;
+    public GameObject ScoreObjects;
+
+	void Start () {
+        RaceUI.SetActive(false);
+        ScoreUI.SetActive(true);
+        AICar.SetActive(false);
+        ScoreObjects.SetActive(true);
+
+	}
+
+    void Update()
+    {
+        InternalScore = CurrentScore;
+        ScoreValue.GetComponent<Text>().text = "" + InternalScore;
+    }
+
+
+}
